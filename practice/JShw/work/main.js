38c8526304e43ffe475da162e78e2668548c8ed1
hevingmey <grisenkov265@gmail.com>
2026-05-18 23:53:39 +0100

    update

---
+++ practice/JShw/work/main.js
@@ -0,0 +1,68 @@
+        // 1
+// let name=prompt("What is your name?");
+// alert('your name is '+ name);
+        //2
+// let age=prompt("When were you born");
+// const year=2026
+// alert(year-age);
+        //3
+// let side=prompt('enter side square');
+// alert('P='+side*4);
+        //4
+// let r=prompt('enter radius circle')
+// alert('S='+3.14*r**2)
+        //5
+// let km=prompt('enter distance')
+// let time = prompt('enter time would you like to arrive')
+// alert('your speed should be '+km/time+' km/h');
+        //6
+// const euro=0.86;
+// let currency=prompt("Enter currency: ");
+// alert('equals currency= '+currency*euro);
+        //7
+// const file=0.82;
+// let gb=Number(prompt('enter size your flash card'))
+//        let counter=0;
+//         while (gb>=0)
+//         {
+//             gb=gb-file;
+//             counter++
+//         }
+// alert('you can save '+counter)
+        //8
+// let sum=Number(prompt('enter sum'));
+// const sum2=Number(prompt('enter price chocolate'));
+//         let counter=0;
+// while(sum>sum2){
+//     sum=sum-sum2;
+//     counter++;
+// }
+// alert('you can buy '+counter+' chocolate and your rest = '+sum);
+        //9
+// let number=Number(prompt("Enter a number"));
+//
+// if(number>999 || number<100){
+//     alert("error");
+// }
+// else
+//         {
+//             let b = (number % 100);
+//             let c = number % 10
+//
+//             let d = (number - b) / 100;
+//
+//             let e = (b - c) / 10;
+//             console.log(d)
+//             console.log(e)
+//             console.log(c)
+//             if (d==c ){
+//                 alert('true')
+//             }
+//             else {
+//                 alert("error");
+//             }
+//         };
+        //10
+// let number=Number(prompt("Enter a number"));
+// number%2===0 &&alert("even number");
+// number%2!==0 &&alert("odd number");
