3a18642025f0bf492132282791e87a4c00937793
hevingmey <grisenkov265@gmail.com>
2026-05-19 12:40:06 +0100

    add hw 2

---
+++ practice/JShw/work/HW2.js
@@ -0,0 +1,53 @@
+        //1
+// let a=Number(prompt('enter first number'));
+// let b=Number(prompt("Enter second number"));
+// let w=0;
+// for (let i = a; i <=b; ++i) {
+//    // console.log(i);
+//    w+=i;
+// };
+//         alert(w);
+        //2
+// let a=Number(prompt("Enter first number"));
+// let b=Number(prompt("Enter second number"));
+//
+// let min=Math.min(a,b);
+// let c=1;
+// for(let i = min; i >=1 ;i--){
+//     if (a%i===0 && b%i===0)
+//     {c=i;
+//     break;
+//     }
+// }
+// alert(c)
+        //3
+//  let a=Number(prompt("Enter number"));
+// for (let i=1;i<=a;i++){
+//     if(a%i===0){
+//         console.log(i);
+//     }
+// }
+        //4
+// let a =prompt("Enter a number");
+// alert(a.length);
+        //5
+// let Pcounter=0;
+// let Mcounter=0;
+// let Ocounter=0;
+// let Parcounter=0;
+// let NePcounter=0;
+// let a=[];
+// for (let i = 0; i <10; i++) {
+//     let w=Number(prompt("Enter a number"));
+//     a.push(w);
+//     if (w>0){
+//         Pcounter++
+//         if (w%2===0){Parcounter++;}
+//     else {NePcounter++;}
+//     }
+//     else if (w<0){Mcounter++}
+//     else if(w===0){Ocounter++;}
+//
+// }
+// alert('you have even : '+Parcounter+' odd : '+NePcounter+' number=0: '+Ocounter+' positive: '+Pcounter+' negatives '+Mcounter)
+
